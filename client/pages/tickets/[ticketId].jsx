b59cbcd3b8344c3e634ee881dbb3680ad2843932
Cesar Boaventura <cesar.boaventura61@gmail.com>
2020-09-14 21:32:51 -0300

    feat: show ticket page

---
+++ client/pages/tickets/[ticketId].jsx
@@ -0,0 +1,44 @@
+import { Alert, AlertIcon, Box, Button, Stack, Text } from '@chakra-ui/core';
+import useRequest from '../../hooks/useRequest';
+
+const TicketShow = ({ ticket }) => {
+  const { request, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: order => console.log(order),
+  });
+
+  console.log({ errors });
+
+  return (
+    <>
+      {errors && <Box m="18px 0 32px">
+        <Stack spacing={3}>
+          {errors.map(error => (
+            <Alert status="error" key={error.field}>
+              <AlertIcon />
+              {error.message}
+            </Alert>
+          ))}
+        </Stack>
+      </Box>}
+      <Text fontSize="4xl">{ticket.title}</Text>
+      <Text fontSize="2xl">Price: {ticket.price}</Text>
+      <Button onClick={request} bg="green.300">
+        Purchase
+      </Button>
+    </>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data };
+};
+
+export default TicketShow;
