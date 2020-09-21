0d955c56c06f31d5a54dbb6d42e3af0101b9408c
Cesar Boaventura <cesar.boaventura61@gmail.com>
2020-09-21 16:48:32 -0300

    feature: docker tests

--- orders/src/routes/__test__/all.test.ts
+++ orders/src/routes/__test__/all.test.ts
@@ -1,21 +1,54 @@
 import request from 'supertest';
 import { getRepository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
 
 import app from '../../app';
 
 import Ticket from '../../models/Ticket';
 
-describe('Routes/index', () => {
-  it('should fetch a list of tickets', async () => {
-    const title = 'valid title';
-    const price = 20.5;
+const buildTicket = async () => {
+  return getRepository(Ticket).save({
+    id: uuidv4(),
+    title: 'show',
+    price: 800,
+  });
+};
 
-    const repo = getRepository(Ticket);
+describe('Routes/all', () => {
+  it('fetches orders for an particular user', async () => {
+    const ticketOne = await buildTicket();
+    const ticketTwo = await buildTicket();
+    const ticketThree = await buildTicket();
 
-    await repo.save({ title, price, userId: 'uuid' });
+    const userOne = global.signin();
+    const userTwo = global.signin();
 
-    const response = await request(app).get('/api/tickets').send();
+    await request(app)
+      .post('/api/orders')
+      .set('Cookie', userOne.session)
+      .send({ ticketId: ticketOne.id })
+      .expect(201);
 
-    expect(response.body).toHaveLength(1);
+    const { body: orderOne } = await request(app)
+      .post('/api/orders')
+      .set('Cookie', userTwo.session)
+      .send({ ticketId: ticketTwo.id })
+      .expect(201);
+    const { body: orderTwo } = await request(app)
+      .post('/api/orders')
+      .set('Cookie', userTwo.session)
+      .send({ ticketId: ticketThree.id })
+      .expect(201);
+
+    const response = await request(app)
+      .get('/api/orders')
+      .set('Cookie', userTwo.session)
+      .expect(200);
+
+    expect(response.body.length).toEqual(2);
+    expect(response.body[0].id).toEqual(orderOne.id);
+    expect(response.body[1].id).toEqual(orderTwo.id);
+    expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+    expect(response.body[1].ticket.id).toEqual(ticketThree.id);
   });
 });
