b0006449f01d2dc9b5a3e2ac2113bede99cfc315
Cesar Boaventura <cesar.boaventura61@gmail.com>
2020-09-21 20:56:16 -0300

    chore: update stripe

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe';
 if (!process.env.STRIPE_KEY) throw new Error('Missing STRIPE_KEY env');
 
 export default new Stripe(process.env.STRIPE_KEY, {
-  apiVersion: '2020-03-02',
+  apiVersion: '2020-08-27',
 });
